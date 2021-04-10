ae37ae2a8086c5cc654831970b380077b8bbc70d
zking63 <zoemking63@gmail.com>
2021-04-10 10:27:41 -0400

    find integers

--- todo1strings.js
+++ todo1strings.js
@@ -1,3 +1,4 @@
+//remove spaces in string
 function myTrim(sent){
     var newS = sent.trim();
     var last = newS[0]
@@ -8,4 +9,17 @@ function myTrim(sent){
     }
     console.log(last)
 }
+//get the integers of a string
+function getDigits(str){
+    var newp = ''
+    for (var i = 0; i < str.length; i++){
+        if (str[i] == "0" || str[i] == "1" || str[i] == "2" || str[i] == "3" || 
+        str[i] == "4" || str[i] == "5" || str[i] == "6" || str[i] == "7" || str[i] == "8"
+        || str[i] == "9"){
+           newp = newp + str[i]
+        }
+    }
+    console.log(newp)
+}
+getDigits("1w4r5uu8")
 myTrim(" Pl ayTha tF u nkyM usi c ")
