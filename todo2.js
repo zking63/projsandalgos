dd2f78b07a5c7aa541847c7569b7cb2b7de22b5f
zking63 <zoemking63@gmail.com>
2021-04-10 09:25:20 -0400

    rotate function

--- todo2.js
+++ todo2.js
@@ -10,3 +10,28 @@ for (var i = arr1.length -2; i > 0; i--){
 }
 newArr.push(firstletter);
 console.log(newArr);
+
+//rotate items in an array by a certain amount 
+function rotate(arr, shift) {
+	let t = Math.abs(shift);
+	while(t > 0) {
+
+		if(shift > 0) {
+			let temp = arr[arr.length-1]
+			
+			for(i = arr.length - 1; i > 0; i--)
+				arr[i] = arr[i-1];
+			arr[0] = temp;
+
+		} else {
+			let temp = arr[0];
+			for(let i = 0; i < arr.length - 1; i++) {
+				arr[i] = arr[i + 1];
+			}
+			arr[arr.length-1] = temp;
+		}
+        t--;
+    }
+    console.log(arr);
+}
+rotate([1,2,3], -2)
