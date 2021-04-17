99666d293b4889efb75fd8c09e5b1eee786dd90d
zking63 <zoemking63@gmail.com>
2021-04-17 15:35:49 -0400

    product added

--- todo1recur.js
+++ todo1recur.js
@@ -7,4 +7,15 @@ function countUp(number){
     }
     console.log(sum);
 }
+//Given num, return the product of ints from 1 up to num. If less than zero, treat as zero. 
+//If not an integer, truncate. Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). 
+//Also, rFact(6.5) = 720 (1*2*3*4*5*6).
+function getproduct(number){
+    var product = 1;
+    for (var i = 1; i < number +1; i++){
+        product = product * i;
+    }
+    console.log(product);
+}
 countUp(3)
+getproduct(3)
