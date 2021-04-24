3cc691fe25d0bc683f90fb3d68288f0a48d12a48
zking63 <zoemking63@gmail.com>
2021-04-24 10:41:37 -0400

    greatest common factor started

--- todo3recur.js
+++ todo3recur.js
@@ -7,4 +7,26 @@ function rBinarySearch(numbers, value){
     }
     return "false";
 }
+//Given two integers, create rGCF(num1,num2) to recursively determine Greatest Common 
+//Factor (the largest integer dividing evenly into both). Greek mathematician Euclid 
+//demonstrated these facts:
+function rGCF(value1, value2){
+    var max = 0;
+    var min = 0;
+    if (value1 > value2){
+        max = value1;
+        min = value2;
+    }
+    else {
+        max = value2;
+        min = value1;
+    }
+    /*for (var i = max; i < max; i--){
+        if (value == numbers[i]){
+            return "true";
+        }
+    }
+    return "false";*/
+}
+rGCF(5, 6);
 console.log(rBinarySearch([1,4,5,6],4))
