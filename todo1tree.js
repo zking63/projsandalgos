0b372eab7bb7f91d7f669013cf4627db74bd1cd9
zking63 <zoemking63@gmail.com>
2021-04-27 16:24:29 -0400

    first tree commit

---
+++ todo1tree.js
@@ -0,0 +1,13 @@
+class BTNode {
+    constructor(value) {
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    // add methods here...
+}
