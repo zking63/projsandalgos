634bcfb7d9ca415995544124921e776ea1462fba
zking63 <zoemking63@gmail.com>
2021-04-29 07:58:11 -0400

    add value function

--- todo1tree.js
+++ todo1tree.js
@@ -9,7 +9,29 @@ class BST {
     constructor() {
         this.root = null;
     }
-   function add(){
-       
+   function add(value){
+       if(this.root){
+           var runner = this.root
+           if(value > runner.value){
+               if(runner.right){
+                   runner = runner.right
+               }
+               else{
+                   runner.right = new Node(value);
+                   return this;
+               }
+           }
+           else{
+               if(runner.left){
+                   runner = runner.left
+               }
+               else{
+                   runner.left = new Node(value);
+                   return this;
+               }
+           }
+       }
+       this.root = new Node(value);
+       return this
    }
 }
