5bdd367df25bba1e544c36bd73d3dbba5e94e663
zking63 <zoemking63@gmail.com>
2021-04-29 08:20:11 -0400

    contains function added

--- todo1tree.js
+++ todo1tree.js
@@ -1,6 +1,6 @@
 class Node {
     constructor(value) {
-        this.val = value;
+        this.value = value;
         this.left = null;
         this.right = null;
     }
@@ -9,33 +9,55 @@ class BST {
     constructor() {
         this.root = null;
     }
-   add(value){
-       if(this.root){
-           var runner = this.root
-           if(value > runner.value){
-               if(runner.right){
+    add(value) {
+        if(this.root) {
+            var runner = this.root;
+            while(runner) {
+                if(value>runner.value) {
+                    if(runner.right){
+                        runner = runner.right;
+                    } else {
+                        runner.right = new Node(value);
+                        return this
+                    }
+                } else {
+                    if(runner.left){
+                        runner = runner.left;
+                    } else {
+                        runner.left = new Node(value);
+                        return this
+                    }
+                }
+            }
+        }
+        this.root = new Node(value);
+        return this
+    }
+
+   //function to see if tree contains value
+   contains(value){
+       var runner = this.root
+       while(runner){
+           if(value == runner.value){
+               return true;
+           }
+           else{
+               if(value > runner.value){
                    runner = runner.right
                }
                else{
-                   runner.right = new Node(value);
-                   return this;
-               }
-           }
-           else{
-               if(runner.left){
                    runner = runner.left
-               }
-               else{
-                   runner.left = new Node(value);
-                   return this;
                }
            }
        }
-       this.root = new Node(value);
-       return this
+       return false
    }
 }
 //create tree
 tree = new BST();
 tree.add(2);
-console.log(tree.add(3));
+tree.add(3);
+tree.add(4);
+tree.add(1);
+console.log(tree.add(5));
+console.log(tree.contains(7));
