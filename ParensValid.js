e051be84b80542bd1eb16a0b333d244cb96661b7
zking63 <zoemking63@gmail.com>
2021-07-19 08:34:19 -0400

    parensvalid started

---
+++ ParensValid.js
@@ -0,0 +1,8 @@
+//create an function that, given an input string, 
+//returns a boolean whether parenthesis in that string are valid
+var input = "y(9)1"
+function Parens(input){
+    for (var i = 0; i < input.length; i++){
+
+    }
+}
