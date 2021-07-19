59b574bc2e7a3f5a42b1d72f86120b52eaad663e
zking63 <zoemking63@gmail.com>
2021-07-19 14:51:47 -0400

    palindrome is printing

---
+++ Palindrome.js
@@ -0,0 +1,12 @@
+//create a function that determines whether a stirng is a palindrome or not
+//do not ignore, spaces, capitals, etc. 
+
+function palindrome(input){
+    var input1 = input;
+    var input2;
+    for (var char of input){
+        console.log(char);
+    }
+}
+
+palindrome("string")
